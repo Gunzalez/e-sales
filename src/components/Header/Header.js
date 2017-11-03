6b9e972afdd7e40a696ecf14b9406111391daf1e
segun.konibire <gunzalez@hotmail.com>
2017-11-03 14:39:19 +0000

    refactors Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,40 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Logo from '../../assets/images/logo.svg';
 import './Header.scss';
-class Header extends Component {
 
-    changeLanguage(lang){
-        this.props.changeLanguage(lang)
-    }
-
-    getDictionaryValue(key){
-        let rtnVal = '';
-        if(this.props.dictionary){
-            rtnVal = this.props.dictionary[key];
-        }
+const getValue = (dictionary, key) => {
+    let rtnVal = '';
+    try {
+        rtnVal = dictionary[key];
+        return rtnVal
+    } catch (err){
         return rtnVal;
     }
+};
 
-    render() {
-        return (
-            <section className="Header bordered">
-                <div className="container">
+const Header = (props) => (
+    <section className="Header bordered">
+        <div className="container">
 
-                    <nav className="navbar">
-                        <div className="navbar-header">
-                            <img src={Logo} alt={this.getDictionaryValue('logo-alt-text')} />
-                            <span>{this.getDictionaryValue('title')}</span>
-                        </div>
-                        <div className="language-controls">
-                            <button onClick={()=> {this.changeLanguage('en')}}>EN</button>
-                            <button onClick={()=> {this.changeLanguage('no')}}>NO</button>
-                        </div>
-                    </nav>
-
+            <nav className="navbar">
+                <div className="navbar-header">
+                    <img src={Logo} alt={ getValue(props.dictionary, 'logo-alt-text') } />
+                    <span>{ getValue(props.dictionary, 'title') } </span>
                 </div>
-            </section>
-        );
-    }
-}
+                <div className="language-controls">
+                    <button onClick={()=> {props.changeLanguage('en')}}>EN</button>
+                    <button onClick={()=> {props.changeLanguage('no')}}>NO</button>
+                </div>
+            </nav>
+
+        </div>
+    </section>
+);
 
 export default Header;
