066e0224a7419129c952aa6c64e0926be763be8b
segun.konibire <gunzalez@hotmail.com>
2017-11-03 20:00:12 +0000

    Adds in some more dictionary entries

--- src/components/Steps/Steps.js
+++ src/components/Steps/Steps.js
@@ -16,9 +16,9 @@ const Steps = (props) => (
                 <button className="button-arrow left"><span></span></button>
                 <button disabled className={props.step === 1 ? 'active' : ''}>{ getValue(props.dictionary, 'stepOne','label') }</button>
                 <button disabled className={props.step === 2 ? 'active' : ''}>{ getValue(props.dictionary, 'stepTwo','label') }</button>
-                <button disabled className={props.step === 3 ? 'active' : ''}>3. Check details</button>
-                <button disabled className={props.step === 4 ? 'active' : ''}>4. Select dealer</button>
-                <button disabled className={props.step === 5 ? 'active' : ''}>5. Sign contract</button>
+                <button disabled className={props.step === 3 ? 'active' : ''}>{ getValue(props.dictionary, 'stepThree','label') }</button>
+                <button disabled className={props.step === 4 ? 'active' : ''}>{ getValue(props.dictionary, 'stepFour','label') }</button>
+                <button disabled className={props.step === 5 ? 'active' : ''}>{ getValue(props.dictionary, 'stepFive','label') }</button>
                 <button disabled className="button-arrow right"><span></span></button>
             </nav>
 
